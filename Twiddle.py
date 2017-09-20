96f44a61ae9a5c8cdb5b43ddd4f85bce5cc89aca
Y1B0 <Y1B0@users.noreply.github.com>
2017-09-20 13:52:43 -0400

    changed twittle added cyclic smooth

--- Twiddle.py
+++ Twiddle.py
@@ -150,40 +150,40 @@ def twiddle(tol=0.2):
     p = [0, 0, 0]
     dp = [1, 1, 1]
     robot = make_robot()
-    x_trajectory, y_trajectory, best_err = run(robot, p)
-
-    it = 0
-    while sum(dp) > tol:
-        print("Iteration {}, best error = {}".format(it, best_err))
+    x_trajectory, y_trajectory, best_err = run(robot,p)
+    it = -1
+    while sum(dp) > 0.2:
+        it += 1
+        print "here",it,best_err
         for i in range(len(p)):
             p[i] += dp[i]
             robot = make_robot()
-            x_trajectory, y_trajectory, err = run(robot, p)
-
+            x,y,err = run(robot,p)
+            
+#            print err,p
             if err < best_err:
+                dp[i] = dp[i]*1.1
                 best_err = err
-                dp[i] *= 1.1
             else:
-                p[i] -= 2 * dp[i]
+                p[i] -= 2*dp[i]
                 robot = make_robot()
-                x_trajectory, y_trajectory, err = run(robot, p)
-
+                x,y,err = run(robot,p)
                 if err < best_err:
                     best_err = err
                     dp[i] *= 1.1
                 else:
                     p[i] += dp[i]
                     dp[i] *= 0.9
-        it += 1
-    print p
     return p
 
 
 params = twiddle()
+#params[2] = 0
 robot = make_robot()
 x_trajectory, y_trajectory, err = run(robot, params)
 n = len(x_trajectory)
+print err,params
 
-plt.plot(x_trajectory, y_trajectory, 'g', label='twiddle PID controller')
-plt.plot(x_trajectory, np.zeros(n), 'r', label='reference')
-plt.legend()
+#plt.plot(x_trajectory, y_trajectory, 'g', label='twiddle PID controller')
+#plt.plot(x_trajectory, np.zeros(n), 'r', label='reference')
+#plt.legend()
